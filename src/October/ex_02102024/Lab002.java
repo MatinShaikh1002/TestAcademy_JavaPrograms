4f044e3858d52714006f94f1d94f3f17c020608f
shaikh.matin.13@gmail.com <shaikh.matin.13@gmail.com>
2024-10-04 07:13:17 +0530

    All files are added

---
+++ src/October/ex_02102024/Lab002.java
@@ -0,0 +1,11 @@
+package October.ex_02102024;
+
+import javax.xml.transform.stream.StreamSource;
+
+public class Lab002 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Test");
+    }
+}
