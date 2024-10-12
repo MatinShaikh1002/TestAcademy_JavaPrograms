1a90266840aae9b5d66c2bac680da9c2f888fe0f
shaikh.matin.13@gmail.com <shaikh.matin.13@gmail.com>
2024-10-12 08:16:02 +0530

    All files are added

---
+++ src/October/ex_10102024/Lab_Literals.java
@@ -0,0 +1,12 @@
+package October.ex_10102024;
+
+public class Lab_Literals {
+
+    public static void main(String[] args) {
+
+
+        int i=100;
+        // 10 is integer literal
+        System.out.println(i);
+    }
+}
---
+++ src/October/ex_10102024/Local_Variable.java
@@ -0,0 +1,12 @@
+package October.ex_10102024;
+
+public class Local_Variable {
+
+    public static void main(String[] args) {
+
+        int age=10;
+
+        System.out.println(age);
+        // can not use this variable, It must assign a value
+    }
+}
